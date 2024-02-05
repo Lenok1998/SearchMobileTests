2418e3ca47d764d59f456ad5af64f6ea270271ff
lenka <lena.malicheva@mail.ru>
2024-02-05 19:47:43 +0400

    fix

--- src/test/java/config/BrowserstackConfig.java
+++ src/test/java/config/BrowserstackConfig.java
@@ -4,7 +4,7 @@ import org.aeonbits.owner.Config;
 @Config.Sources(
         {"classpath:mobile.properties"}
 )
-public interface browserstackConfig extends Config {
+public interface BrowserstackConfig extends Config {
 
     @Key("user")
     String getUser();
--- src/test/java/drivers/BrowserstackDriver.java
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -1,6 +1,6 @@
 package drivers;
 
-import config.browserstackConfig;
+import config.BrowserstackConfig;
 import com.codeborne.selenide.WebDriverProvider;
 import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.Capabilities;
@@ -14,31 +14,29 @@ import java.net.URL;
 
 public class BrowserstackDriver implements WebDriverProvider {
 
-    static browserstackConfig config = ConfigFactory.create(browserstackConfig.class, System.getProperties());
+    static BrowserstackConfig config = ConfigFactory.create(BrowserstackConfig.class, System.getProperties());
     @Nonnull
     @Override
     public WebDriver createDriver(@Nonnull Capabilities capabilities) {
         MutableCapabilities caps = new MutableCapabilities();
-        // caps.merge(capabilities);
 
-        // Set your access credentials
+
+
         caps.setCapability("browserstack.user",  config.getUser());
         caps.setCapability("browserstack.key", config.getKey());
 
-        // Set URL of the application under test
+
         caps.setCapability("app", config.getApp());
 
-        // Specify device and os_version for testing
+
         caps.setCapability("device", config.getDevice());
         caps.setCapability("os_version", config.getOsVersion());
 
-        // Set other BrowserStack capabilities
+
         caps.setCapability("project", config.getProject());
         caps.setCapability("build", config.getBuild());
         caps.setCapability("name", config.getName());
 
-        // Initialise the remote Webdriver using BrowserStack remote URL
-        // and desired capabilities defined above
         try {
             return new RemoteWebDriver(
                     new URL(config.getBaseUrl()), caps);
